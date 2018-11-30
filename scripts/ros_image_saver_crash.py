8a1a04852afecea78f1fb231e2e4a17cec9a90b2
Elijah S. Lee <elijahsjlee@gmail.com>
2018-11-30 12:32:32 -0500

    initial commit

---
+++ scripts/ros_image_saver_crash.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+# Copyright (c) 2015, Rethink Robotics, Inc.
+
+# Using this CvBridge Tutorial for converting
+# ROS images to OpenCV2 images
+# http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+
+# Using this OpenCV2 tutorial for saving Images:
+# http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
+
+# rospy for the subscriber
+import rospy
+# ROS Image message
+from sensor_msgs.msg import CompressedImage
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+# OpenCV2 for saving an image
+import cv2
+import numpy as np
+
+# Instantiate CvBridge
+bridge = CvBridge()
+i = 10680 # 1
+
+def image_callback(msg):
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        np_arr = np.fromstring(msg.data, np.uint8)
+        cv2_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        #      cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        # Save your OpenCV2 image as a jpeg 
+        global i
+        cv2.imwrite('camera_image_{}.jpeg'.format(str(i).zfill(5)), cv2_img)
+        i = i - 1  # i+1
+
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    image_topic = "dragonfly13/stereo/left/image_radiation/compressed"
+    # Set up your subscriber and define its callback
+    rospy.Subscriber(image_topic, CompressedImage, image_callback)
+    # Spin until ctrl + c
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
